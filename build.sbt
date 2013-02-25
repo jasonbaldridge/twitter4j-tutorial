93446148a273a184442a3364fd9b70f4e98b8b30
Jason Baldridge <jasonbaldridge@gmail.com>
2013-02-25 15:42:23 -0600

    Checked in code from first twitter4j tutorial.

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "twitter4j-tutorial"
+
+version := "0.1.0 "
+
+scalaVersion := "2.10.0"
+
+libraryDependencies += "org.twitter4j" % "twitter4j-stream" % "3.0.3"
---
+++ src/main/scala/TwitterStream.scala
@@ -0,0 +1,80 @@
+package bcomposes.twitter
+
+import twitter4j._
+
+object StatusStreamer {
+  def main(args: Array[String]) {
+    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+    twitterStream.addListener(Util.simpleStatusListener)
+    twitterStream.sample
+    Thread.sleep(2000)
+    twitterStream.cleanUp
+    twitterStream.shutdown
+  }
+}
+
+object FollowIdsStreamer {
+  def main(args: Array[String]) {
+    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+    twitterStream.addListener(Util.simpleStatusListener)
+    twitterStream.filter(new FilterQuery(Array(1344951,5988062,807095,3108351)))
+    Thread.sleep(10000)
+    twitterStream.cleanUp
+    twitterStream.shutdown
+  }
+}
+
+object SearchStreamer {
+  def main(args: Array[String]) {
+    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+    twitterStream.addListener(Util.simpleStatusListener)
+    twitterStream.filter(new FilterQuery().track(args))
+    Thread.sleep(10000)
+    twitterStream.cleanUp
+    twitterStream.shutdown
+  }
+}
+
+object AustinStreamer {
+  def main(args: Array[String]) {
+    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+    twitterStream.addListener(Util.simpleStatusListener)
+    val austinBox = Array(Array(-97.8,30.25),Array(-97.65,30.35))
+    twitterStream.filter(new FilterQuery().locations(austinBox))
+    Thread.sleep(10000)
+    twitterStream.cleanUp
+    twitterStream.shutdown
+  }
+}
+
+object LocationStreamer {
+  def main(args: Array[String]) {
+    val boundingBoxes = args.map(_.toDouble).grouped(2).toArray
+    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+    twitterStream.addListener(Util.simpleStatusListener)
+    twitterStream.filter(new FilterQuery().locations(boundingBoxes))
+    Thread.sleep(10000)
+    twitterStream.cleanUp
+    twitterStream.shutdown
+  }
+}
+
+object Util {
+
+  val config = new twitter4j.conf.ConfigurationBuilder()
+    .setOAuthConsumerKey("[your consumer key here]")
+    .setOAuthConsumerSecret("[your consumer secret here]")
+    .setOAuthAccessToken("[your access token here]")
+    .setOAuthAccessTokenSecret("[your access token secret here]")
+    .build
+
+  def simpleStatusListener = new StatusListener() {
+    def onStatus(status: Status) { println(status.getText) }
+    def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
+    def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
+    def onException(ex: Exception) { ex.printStackTrace }
+    def onScrubGeo(arg0: Long, arg1: Long) {}
+    def onStallWarning(warning: StallWarning) {}
+  }
+
+}
